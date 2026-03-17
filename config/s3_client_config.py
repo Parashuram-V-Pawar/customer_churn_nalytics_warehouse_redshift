6be243bcec3901fac051aec9fb2617b41836f40a
Parashuram <parashurampawar56@gmail.com>
2026-03-17 16:39:09 +0530

    modify program: [parashuram] updated python program to configure s3 client

--- config/s3_client_config.py
+++ config/s3_client_config.py
@@ -11,11 +11,21 @@ load_dotenv()
 # AWS configuration
 # -----------------------------------------------------------------------
 AWS_REGION = os.getenv("AWS_REGION")
-BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 # -----------------------------------------------------------------------
 # client initialization functions
 # -----------------------------------------------------------------------
+s3_client = None
 def get_s3_client():
-    session = boto3.Session(region_name=AWS_REGION)
-    return session.client("s3")
+    global s3_client
+    if s3_client is None:
+        s3_client = boto3.client("s3", region_name=AWS_REGION)
+    return s3_client
+
+
+redshift_client = None
+def get_redshift_client():
+    global redshift_client
+    if redshift_client is None:
+        redshift_client = boto3.client("redshift-data", region_name=AWS_REGION)
+    return redshift_client
